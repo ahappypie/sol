1c339178fd8d2667c3105088c10cc7dda9f86b2e
ahappypie <bbagdasarian@nativo.com>
2020-03-02 12:45:17 -0800

    init minerva

--- rook/05-dashboard-external.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-kubectl -n rook-ceph exec -it $(kubectl -n rook-ceph get pod -l "app=rook-ceph-tools" -o jsonpath='{.items[0].metadata.name}') -- ceph config set mgr mgr/dashboard/server_addr 0.0.0.0
-
-kubectl delete pod -n rook-ceph  $(kubectl -n rook-ceph get pod -l "app=rook-ceph-mgr" -o jsonpath='{.items[0].metadata.name}')
