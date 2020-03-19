13bb6e396ff8d12b9feeff9c3c3579da00c4e0a1
ahappypie <bbagdasarian@nativo.com>
2020-03-19 09:41:55 -0700

    ICOF

--- minikube.sh
+++ minikube.sh
@@ -2,8 +2,8 @@
 minikube start \
 --cpus 4 \
 --disk-size 30000 \
---kubernetes-version 1.17.3 \
---memory 12288 \
---vm-driver hyperkit \
+--kubernetes-version 1.17.4 \
+--memory 16384 \
+--driver hyperkit \
 --network-plugin=cni \
 --extra-config=kubelet.network-plugin=cni
